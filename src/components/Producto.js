421ca0bbe74a6d42269dd3a27df3b5fe34d9b848
jgoyeneche <jgoyeneche@alumnos.uai.cl>
2020-05-23 18:09:39 -0400

    v1.0

---
+++ src/components/Producto.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Producto = ({producto, productos, carrito, agregarProducto}) => {
+
+    const{nombre, precio,id} = producto;
+
+    //Agregar producto al carrito
+    const seleccionarProducto = id => {
+        const producto = productos.filter(producto => producto.id === id)[0];
+        console.log(producto);
+    }
+
+    return ( 
+        <div>
+            <h2>{nombre}</h2>
+            <p>Precio: ${precio}</p>
+            <button
+                type="button"
+                onClick={ () => seleccionarProducto(id)}
+                >Comprar</button>
+        </div>
+    );
+}
+ 
+export default Producto;
